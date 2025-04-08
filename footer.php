d88b624b4511812c54ae245fddda11ff98ed475c
gsimao1 <gabriela.scapovilla@gmail.com>
2025-04-08 08:11:52 -0300

    footer.php

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer>
+    <div class="container">
+        <div class="footer-content">
+            <div class="footer-column">
+                <h3>QuadToys</h3>
+                <p>O maior portal de colecionáveis do Brasil. Conectamos colecionadores e entusiastas em uma comunidade apaixonada.</p>
+                <div class="social-links">
+                    <a href="#">F</a>
+                    <a href="#">I</a>
+                    <a href="#">T</a>
+                    <a href="#">Y</a>
+                </div>
+            </div>
+            <div class="footer-column">
+                <h3>Navegação</h3>
+                <ul>
+                    <li><a href="index.php">Início</a></li>
+                    <li><a href="#">Categorias</a></li>
+                    <li><a href="#">Destaques</a></li>
+                    <li><a href="#">Raridades</a></li>
+                    <li><a href="#">Comunidade</a></li>
+                    <li><a href="contato.php">Contato</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            &copy; 2025 QuadToys - Todos os direitos reservados
+        </div>
+    </div>
+</footer>
+</body>
+</html>
